1f8b06dfd7aa6dc7377962c4d2a56700cb3955ee
petterdybdal <petter.dybdal@gmail.com>
2018-02-26 17:06:26 +0100

    Add list of bands in drawer

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -15,15 +15,11 @@ import Button from 'material-ui/Button';
 const styles = theme => ({
   root: {
     width: 300,
+    height: '100vh',
     backgroundColor: theme.palette.background.paper,
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'flex-start',
-  },
-  signout: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignSelf: 'center',
   },
   input: {
     display: 'none',
@@ -40,6 +36,9 @@ const styles = theme => ({
       backgroundColor: 'rgb(230, 0, 80)',
     },
   },
+  bands: {
+    height: '100%',
+  }
 });
 
 class TemporaryDrawer extends React.Component {
@@ -55,7 +54,7 @@ class TemporaryDrawer extends React.Component {
 
   render() {
     const { classes } = this.props;
-
+    const bands = this.props.bands.map(band => <ListItem key={band.name}>{band.name}</ListItem>);
     return (
       <div>
       <IconButton color='default' onClick={this.toggleDrawer('left', true)}>
@@ -78,25 +77,24 @@ class TemporaryDrawer extends React.Component {
                 </ListItem>
               </List>
               <Divider />
-              <List>
+              <List className={classes.bands}>
                 <ListItem>
                   <ListItemIcon>
                     <GroupIcon />
                   </ListItemIcon>
                   <ListItemText primary='Bands' />
                 </ListItem>
-              </List>
-              <Divider />
-              <List className={classes.joincreate}>
-                <Button variant='raised' color='default' className={classes.button}>
-                  Join band
-                </Button>
-                <Button variant='raised' color='default' className={classes.button}>
-                  Create band
-                </Button>
-              </List>
-              <Divider />
-              <List className={classes.signout}>
+                <div className={classes.joincreate}>
+                  <Button variant='raised' color='default' className={classes.button}>
+                    Join band
+                  </Button>
+                  <Button variant='raised' color='default' className={classes.button}>
+                    Create band
+                  </Button>
+                </div>
+                <List>
+                  {bands}
+                </List>
               </List>
               <Divider />
               <Button onClick={() => this.props.onSignOut()} variant='raised' color='default' className={classes.buttonSignout}>
