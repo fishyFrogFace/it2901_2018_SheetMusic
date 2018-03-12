5b78ff73f3833a669d96516bca33b2a7e4aa75d1
Kristian Stamland <kristiansta@hotmail.com>
2018-03-12 16:36:48 +0100

    Commented out tests

--- hosting/src/__tests__/Setlist.test.js
+++ hosting/src/__tests__/Setlist.test.js
@@ -1,10 +1,10 @@
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
+// import configureMockStore from 'redux-mock-store';
+// import thunk from 'redux-thunk';
 
-const middlewares = [thunk];
-const mockStore = configureMockStore(middlewares);
+// const middlewares = [thunk];
+// const mockStore = configureMockStore(middlewares);
 
-describe('Setlist', () => {
-    xit('works', () => {
-    })
-});
+// describe('Setlist', () => {
+//     xit('works', () => {
+//     })
+// });
