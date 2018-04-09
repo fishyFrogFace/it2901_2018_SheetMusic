f723a487fa5e9c48476e682ee72a54323a4aad57
Brage Nilsson <brage.son@gmail.com>
2018-04-09 14:08:37 +0200

    Minor fix

--- hosting/src/containers/Score.js
+++ hosting/src/containers/Score.js
@@ -5,7 +5,7 @@ import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 
-import {IconButton, Menu, MenuItem, Select, Snackbar} from "material-ui";
+import {Drawer, IconButton, Menu, MenuItem, Select, Snackbar} from "material-ui";
 import ArrowBackIcon from 'material-ui-icons/ArrowBack';
 import MoreVertIcon from 'material-ui-icons/MoreVert';
 
@@ -13,7 +13,7 @@ import firebase from 'firebase';
 import 'firebase/storage';
 
 import DownloadSheetsDialog from "../components/dialogs/DownloadSheetsDialog";
-import {FileDownload} from "material-ui-icons";
+import {FileDownload, Info, InfoOutline} from "material-ui-icons";
 
 const styles = {
     root: {},
@@ -32,6 +32,10 @@ const styles = {
 
     sheet: {
         width: '100%'
+    },
+
+    drawer__paper: {
+        width: 250
     }
 };
 
@@ -188,8 +192,11 @@ class Score extends React.Component {
                             </Select>
                         }
                         <div className={classes.flex}/>
-                        <IconButton color="inherit" onClick={e => this._onMenuClick('download')}>
+                        <IconButton color='inherit' onClick={e => this._onMenuClick('download')}>
                             <FileDownload/>
+                        </IconButton>
+                        <IconButton color='inherit' onClick={e => this._onMenuClick('info')}>
+                            <InfoOutline/>
                         </IconButton>
                     </Toolbar>
                 </AppBar>
@@ -201,6 +208,13 @@ class Score extends React.Component {
                         )
                     }
                 </div>
+                <Drawer
+                    anchor='right'
+                    open={false}
+                    classes={{paper: classes.drawer__paper}}
+                >
+                    lol
+                </Drawer>
                 <DownloadSheetsDialog onRef={ref => this.downloadDialog = ref}/>
                 <Snackbar
                     anchorOrigin={{
